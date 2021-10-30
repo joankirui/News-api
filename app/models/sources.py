3e19a6fde078d7d60195b69b833cfa8d4d2c37a0
joankirui <joankirui99@gmail.com>
2021-10-30 15:06:59 +0300

    add code to make SourceTest pass

---
+++ app/models/sources.py
@@ -0,0 +1,11 @@
+class Sources:
+    '''
+    Sources class to define Sources Objects
+    '''
+
+    def __init__(self,id,name,description,url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        
