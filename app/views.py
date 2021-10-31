365a9a46479732b772232f79c969ae1f5240396c
joankirui <joankirui99@gmail.com>
2021-10-31 11:25:33 +0300

    styled the main page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_sources
+from .request import get_sources,get_articles
 
 # Views
 @app.route('/')
@@ -14,3 +14,12 @@ def index():
     business_news = get_sources('business')
     sports_news = get_sources('sports')
     return render_template('index.html', title = title,general=general_news,business=business_news,sports=sports_news)
+@app.route('/')
+def NewsArticles():
+    """
+    View that would return news articles
+     
+    """
+    health_articles = get_articles('health')
+    education_articles = get_articles('technology')
+    return render_template('articles.html',health=health_articles, tech =education_articles)
